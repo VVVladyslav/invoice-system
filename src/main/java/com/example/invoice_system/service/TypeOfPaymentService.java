7c2e924eeed75f490bc8a3c0c2c239a3871be8d1
VVVladyslav <vladoszhar@gmail.com>
2025-03-29 21:36:09 +0300

    какойто слежущий шаг

--- src/main/java/com/example/invoice_system/service/TypeOfPaymentService.java
+++ src/main/java/com/example/invoice_system/service/TypeOfPaymentService.java
@@ -2,7 +2,11 @@ package com.example.invoice_system.service;
 
 import com.example.invoice_system.model.TypeOfPayment;
 import com.example.invoice_system.repository.TypeOfPaymentRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import org.springframework.cache.annotation.CacheEvict;
+
 
 import java.util.List;
 
@@ -15,14 +19,17 @@ public class TypeOfPaymentService {
         this.repository = repository;
     }
 
+    @Cacheable("typeOfPayments")
     public List<TypeOfPayment> getAll() {
         return repository.findAll();
     }
 
+    @CacheEvict(value = "typeOfPayments", allEntries = true)  // Очищаем кэш, если данные изменяются
     public TypeOfPayment save(TypeOfPayment type) {
         return repository.save(type);
     }
 
+    @CacheEvict(value = "typeOfPayments", allEntries = true)
     public void deleteById(Long id) {
         repository.deleteById(id);
     }
