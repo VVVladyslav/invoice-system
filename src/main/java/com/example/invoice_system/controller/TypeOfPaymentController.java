4b9199002b028c1790a9e0bcde18965b57c30ba7
VVVladyslav <vladoszhar@gmail.com>
2025-03-29 22:39:07 +0300

    какойто слежущий шаг

--- src/main/java/com/example/invoice_system/controller/TypeOfPaymentController.java
+++ src/main/java/com/example/invoice_system/controller/TypeOfPaymentController.java
@@ -2,6 +2,8 @@ package com.example.invoice_system.controller;
 
 import com.example.invoice_system.model.TypeOfPayment;
 import com.example.invoice_system.service.TypeOfPaymentService;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,16 +18,19 @@ public class TypeOfPaymentController {
         this.service = service;
     }
 
+    @Cacheable(value = "typesCache") // Аннотация для кэширования данных
     @GetMapping
     public List<TypeOfPayment> getAll() {
         return service.getAll();
     }
 
+    @CacheEvict(value = "typesCache", allEntries = true) // Очистка кэша при добавлении нового типа оплаты
     @PostMapping
     public TypeOfPayment create(@RequestBody TypeOfPayment type) {
         return service.save(type);
     }
 
+    @CacheEvict(value = "typesCache", allEntries = true) // Очистка кэша при удалении типа оплаты
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.deleteById(id);
